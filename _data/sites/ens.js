320d6ed984238d90f1f3b31271bea2a9a0ec7977
Anouar Dhawadi <anouardhawadi@gmail.com>
2024-10-11 14:12:00 +0200

    Update ens.js  to pp

--- _data/sites/ens.js
+++ _data/sites/ens.js
@@ -1,29 +1,12 @@
 module.exports = {
   name: "Ens", // optional, falls back to object key
-  description: "Ens paris sacly site  : Liste 1",
+  description: "Ens paris sacly site preprod",
   options: {
     runs: 1,
     frequency: 1, // (in minutes)
   },
   urls: [
-    "https://ens-paris-saclay.fr/",
-    "https://ens-paris-saclay.fr/admission/concours/concours-1e-annee",
-    "https://ens-paris-saclay.fr/formations/masters",
-    "https://ens-paris-saclay.fr/admission",
-    "https://ens-paris-saclay.fr/admission/admission-sur-concours",
-    "https://ens-paris-saclay.fr/admission/sur-concours/concours-dadmission-en-1ere-annee/concours-psi",
-    "https://ens-paris-saclay.fr/admission/admission-sur-dossier",
-    "https://ens-paris-saclay.fr/admission/sur-dossier/voie-etudiante",
-    "https://ens-paris-saclay.fr/Formations",
-    "https://ens-paris-saclay.fr/admission/sur-concours/concours-dadmission-en-1ere-annee/concours-eco-gestion-option-i",
-    "https://ens-paris-saclay.fr/admission/inscription-et-reinscription",
-    "https://ens-paris-saclay.fr/entites/departements",
-    "https://ens-paris-saclay.fr/etude/diplome-ens-paris-saclay",
-    "https://ens-paris-saclay.fr/admission/concours/concours-pre-master/annales-concours-pre-master",
-    "https://ens-paris-saclay.fr/admission/sur-concours/concours-dadmission-en-1ere-annee/concours-pc",
-    "https://ens-paris-saclay.fr/bibliotheque/ressources-documentaires",
-    "https://ens-paris-saclay.fr/admission/sur-concours/concours-dadmission-en-1ere-annee/concours-mp",
-    "https://ens-paris-saclay.fr/actualite/bienvenue-aux-candidats-admissibles-2023",
-    "https://ens-paris-saclay.fr/actualite",
+    "https://ens-paris-saclay-preprod.ci.itdev.fr",
+    "https://ens-paris-saclay-preprod.ci.itdev.fr/empty-page"
   ]
 };
